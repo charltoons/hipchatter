c958c65a0da23ea8a87e37159fab902ed2520036
Charlton Roberts <charlton.roberts@pilot.is>
2013-11-12 13:12:44 -0500

    url default

--- test/test.js
+++ test/test.js
@@ -1,9 +1,36 @@
-var assert = require("assert")
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
+var assert = require("assert");
+var chai = require("chai");
+var expect = chai.expect;
+var colors = require("colors");
+
+// Make sure the API Credentials are present
+try { var settings = require(__dirname+"/settings.json"); } 
+catch (e) { console.error('Create test/settings.json and populate with your credentials.'.red);}
+
+// Setup hipchatter
+var Hipchatter = require(__dirname+'/../hipchatter.js');
+var hipchatter;
+
+
+describe('Creating hipchatter object', function(){
+  describe('hipchatter', function(){
+    hipchatter = new Hipchatter(settings.apikey);
+    it('should exists and be an object', function(){
+      expect(hipchatter).to.exist;
+      expect(hipchatter).to.be.an('object');
+    });
+  });
+});
+
+
+/** HELPERS **/
+describe('Helpers', function(){
+    describe('url default', function(){
+        var url = hipchatter.url('room/history');
+        it('should return a valid URL with the auth token', function(){
+            expect(url).to.be.a('string');
+            expect(url).to.contain(settings.apikey);
+            expect(url).to.contain('hipchat');
+        });
+    });
+});
