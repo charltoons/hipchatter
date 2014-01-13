1a4e45e10df5b19d540db2108d1ef91ff6479126
Charlton Roberts <charlton.roberts@pilot.is>
2014-01-13 10:50:17 -0500

    Error messages in example.js

--- example.js
+++ example.js
@@ -7,22 +7,24 @@ var key = process.argv[2];
 var notify_key = process.argv[3];
 var hipchatter = new Hipchatter(key);
 
+if (key === undefined || notify_key === undefined) {
+    console.error('Please include your API key and Notification API key as command line arguments.');
+    console.error('eg. node example.js 123456789abcdef 987654321fedcba');
+    process.exit(1);
+}
+
 //sample method
 hipchatter.rooms(function(err, rooms){
     if (err) console.log(err);
     console.log(rooms);
 });
 
-// hipchatter.history('HipChatter', function(err, rooms){
-//     console.log(rooms);
-// });
-
-hipchatter.emoticons({}, function(err, emoticons){
-    console.log(emoticons);
+hipchatter.history('HipChatter', function(err, rooms){
+    console.log(rooms);
 });
 
-// hipchatter.notify('HipChatter', '<code>Such Notifications. Many HipChat. Wow.</code>', notify_key, function(err, result){
-//     if (err == undefined) console.log(err);
-//     else console.log(results);
-// });
+hipchatter.notify('HipChatter', '<code>Such Notifications. Many HipChat. Wow.</code>', notify_key, function(err, result){
+    if (err == undefined) console.log(err);
+    else console.log(results);
+});
 
