bd3d62e62df9f9e5cc5851fe4d03dce9b31d8827
Maik Diepenbroek <m.diepenbroek@avisi.nl>
2014-02-15 14:54:58 +0100

    Implemented invite user

--- test/test.js
+++ test/test.js
@@ -386,6 +386,32 @@ describe('Endpoints', function(){
         
     });
 
+    // Add a member to a room
+    describe('Invite a member to a room', function() {
+        var err, response, params;
+        params = {
+            room_name: settings.test_room, 
+            user_email: 'testuser@testuser.com'
+        };
+
+        before(function(done) {
+            hipchatter.invite_member(params, {reason: 'We wanted to invite you to this room'}, function(_err, _body, _response) {
+                err = _err;
+                response = _response;
+                done();
+            });
+        });
+
+        it('should not return an error', function() {
+            expect(err).to.be.null;
+        });
+
+        it('should return status code 204 when the user is succesfully invited to the room', function() {
+            expect(response).to.equal(204);
+        });
+        
+    });
+
      // Remove a member from a room
     describe('Remove a member from a room', function() {
         var err, response, params;
