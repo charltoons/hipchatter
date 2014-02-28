7e2b1bd64fc3b4d6768dedfc36c9dc07d5f2f381
Charlton Roberts <charlton.roberts@pilot.is>
2014-02-28 17:02:33 -0500

    Added [4mRunning "execute:stub" (execute) task[24m [36m-> [39mexecuting [36m/Users/croberts1/Projects/npm/hipchatter/utils/create_test_room.js[39m [31m[31m
    Error creating room:[39m [Error: Another room exists with that name.]
    
    [39m[36m-> [39mcompleted [36m/Users/croberts1/Projects/npm/hipchatter/utils/create_test_room.js[39m (699ms)
    
    [32m>> [39m1 file and 0 calls executed (701ms)
    
    [32mDone, without errors.[39m for creating a test room for the tests to operate on

---
+++ utils/create_test_room.js
@@ -0,0 +1,15 @@
+// Make sure the API Credentials are present
+try { var settings = require(__dirname+"/../test/settings.json"); } 
+catch (e) { console.error('Create test/settings.json and populate with your credentials.'.red);}
+
+var colors = require('colors')
+
+// Setup hipchatter
+var Hipchatter = require(__dirname+'/../hipchatter.js');
+var hipchatter = new Hipchatter(settings.apikey);
+
+// Create a room to run tests from
+hipchatter.create_room({name : settings.test_room}, function(err, room){
+    if (err == null) console.log(('\n\nCreated "'+settings.test_room+'" room with id: '+room.id+'\n\n').green);
+    else console.error('\n\nError creating room:'.red, err, '\n\n');
+});
